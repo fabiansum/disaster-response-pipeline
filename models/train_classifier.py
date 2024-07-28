3cb5d6949ccc21bb8cdbbf712b031684a04ce678
fabiansum <fab.sumarna@gmail.com>
2024-07-28 15:19:45 +0800

    Add functions and PEP8 docstring in train_classifier.py and __init__.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,111 @@
 import sys
+import pandas as pd
+import numpy as np
+import pickle
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_multilabel_classification
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+import joblib
 
+nltk.download(['punkt', 'wordnet'])
 
 def load_data(database_filepath):
-    pass
+    """
+    Load data from a SQLite database.
+
+    Parameters:
+    database_filepath (str): File path of the SQLite database.
+
+    Returns:
+    X (Series): Series containing the messages.
+    y (DataFrame): DataFrame containing the categories.
+    category_names (Index): Index object containing the category names.
+    """    
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql("SELECT * FROM disaster_response", engine)
+    X = df['message']
+    y = df.iloc[:, 4:]
+    category_names = y.columns 
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+    Tokenize and lemmatize the input text.
+
+    Parameters:
+    text (str): Text to be tokenized and lemmatized.
+
+    Returns:
+    clean_tokens (list): List of clean tokens.
+    """    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Build a machine learning pipeline and perform grid search.
+
+    Returns:
+    model (GridSearchCV): Grid search model object.
+    """    
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
+])
+    parameters = {        
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4]
+    }
+
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluate the model on test data and print the classification report for each category.
+
+    Parameters:
+    model: Trained model.
+    X_test (DataFrame): Test features.
+    Y_test (DataFrame): True labels for test data.
+    category_names (list): List of category names.
+    """    
+    y_pred = model.predict(X_test)
+    for index, column in enumerate(Y_test):
+        print(column) 
+        print(classification_report(Y_test[column], y_pred[:, index]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Save the trained model to a file.
+
+    Parameters:
+    model: Trained model.
+    model_filepath (str): File path to save the model.
+    """    
+    filename = model_filepath
+    joblib.dump(model, open(filename, 'wb'))
 
 
 def main():
