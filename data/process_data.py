183c61772ec1c54cb8e5ed640e1f7aed20eefeb9
fabiansum <fab.sumarna@gmail.com>
2024-07-28 15:18:34 +0800

    Add functions and PEP8 docstring in process_data.py and __init__.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,76 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load data from CSV files and merge them into a single DataFrame.
+    
+    Parameters:
+    messages_filepath (str): File path to the messages CSV file.
+    categories_filepath (str): File path to the categories CSV file.
+    
+    Returns:
+    df (DataFrame): Merged DataFrame containing messages and categories.
+    """
+    # Read messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # Read categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # Merge datasets
+    df = messages.merge(categories, how='outer', on='id')
+    
+    return df
+    
 
 
 def clean_data(df):
-    pass
+    """
+    Clean the input DataFrame by splitting categoriesm converting category values to binary, and removing duplicates and invalid entries.
+    
+    Parameters:
+    df (DataFrame): DataFrame containing the merged messages and categories data.
+    
+    Returns:
+    df (DataFrame): Cleaned DataFrame with separate category columns and binary values.
+    """
+    # Split categories into separate category columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # Extract column names from the first row
+    category_colnames = categories.iloc[0].apply(lambda x: x.split('-')[0])
+    
+    # Rename the columns
+    categories.columns = category_colnames
+    
+    # Convert category values to 0 or 1
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1].astype(int)
+    
+    # Drop the original categories column and concatenate the new one
+    df = pd.concat([df.drop('categories', axis=1), categories], axis=1)
+    
+    # Drop duplicates
+    df = df.drop_duplicates()
+
+    # Remove rows containing '2' in any column
+    df = df[~(df == 2).any(axis=1)]
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the cleaned DataFrame to a SQLite database.
+    
+    Parameters:
+    df (DataFrame): Cleaned DataFrame to be saved.
+    database_filename (str): File path for the SQLite database.
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('disaster_response', engine, index=False, if_exists='replace')  
 
 
 def main():
